6a400b27cc59db7c8087087c31490bf2a6e7964b
akmonk <akmonk@mail.ru>
2015-12-24 22:09:29 +0400

    package patch envx

---
+++ envx.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+int main(int argc, char *argv[])
+
+{
+  int i;
+  char *pid;
+  // переданы ли pid
+  if (argc < 2)
+  {
+    printf("Ошибка. Введите pwdx pid1 [pid2]\n");
+    return 1;
+  }
+  else
+  {
+    for (i=1;i<argc;i++)
+    envx(argv[i]);
+  }
+  return 0;
+} 
